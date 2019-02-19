5eb0acc26f4646eec48582c97fab0ae344374b13
Elisaveta <elisaveta.s.hristova@gmail.com>
2019-02-19 22:11:10 +0200

    Recipe app

---
+++ forkify/src/js/config.js
@@ -0,0 +1,7 @@
+// key = 6157c64540bbe53fe4fff7dc77bd7099 
+// search = https://www.food2fork.com/api/search
+
+export const proxy = "https://crossorigin.me/";
+export const key = "c559155b95277bd12c2f183367b62df8";
+// export const key = "e13517bcd6518147818d2b7596e82317";
+// export const key = "6157c64540bbe53fe4fff7dc77bd7099";
